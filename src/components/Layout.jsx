bae68cbd8f442bb35ce6e30232b42f8fa157b261
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-26 16:06:09 +0430

    updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -81,7 +81,7 @@ const Layout = ({ children }) => {
                     <path d="M4 42.4379C4 42.4379 14.0962 36.0744 24 41.1692C35.0664 46.8624 44 42.2078 44 42.2078L44 7.01134C44 7.01134 35.068 11.6577 24.0031 5.96913C14.0971 0.876274 4 7.27094 4 7.27094L4 42.4379Z"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg sm:text-xl font-bold leading-tight tracking-[-0.015em] bg-gradient-to-r from-white to-blue-200 bg-clip-text text-transparent hidden xs:block">
+                <h2 className="text-lg sm:text-xl font-bold leading-tight tracking-[-0.015em] bg-gradient-to-r from-white to-blue-200 bg-clip-text text-transparent">
                   KPU Alumni
                 </h2>
               </div>
