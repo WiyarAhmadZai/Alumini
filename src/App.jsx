bf21bc390c42941c7aba383e93b80296af5b2dad
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-26 17:22:21 +0430

    the app file routes are updted

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import DirectoryPage from './pages/DirectoryPage';
+import JobBoard from './pages/JobBoard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/directory" element={<DirectoryPage />} />
+        <Route path="/jobs" element={<JobBoard />} />
       </Routes>
     </div>
   );
