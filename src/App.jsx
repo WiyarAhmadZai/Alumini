d0e6b4c720aaab67a298899c0a3c523f09ea0506
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-26 16:12:13 +0430

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import DirectoryPage from './pages/DirectoryPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/directory" element={<DirectoryPage />} />
       </Routes>
     </div>
   );
