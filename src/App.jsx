936f609392dd81ad0d30ff1f02c4ed36d69564a3
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-01-26 14:20:54 +0430

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/auth/LoginPage';
-import RegisterPage from './pages/auth/RegisterPage';
-import ForgotPasswordPage from './pages/auth/ForgotPasswordPage';
-import DashboardPage from './pages/dashboard/DashboardPage';
-import AlumniDirectoryPage from './pages/directory/AlumniDirectoryPage';
-import EventManagementPage from './pages/events/EventManagementPage';
-import ProfilePage from './pages/profile/ProfilePage';
-import GraduatedStudentsPage from './pages/GraduatedStudentsPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/directory" element={<AlumniDirectoryPage />} />
-        <Route path="/events" element={<EventManagementPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/graduated-students" element={<GraduatedStudentsPage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
   );
