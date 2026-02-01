22434064668b656fd59511b04a957d634496e76c
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-01 17:09:24 +0430

    map section updated

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -97,9 +97,9 @@ const ContactPage = () => {
               <div className="bg-white rounded-xl shadow-lg overflow-hidden">
                 <div className="relative">
                   {/* Embedded Map */}
-                  <div className="w-full h-[400px] bg-gray-200 relative">
+                  <div className="w-full h-[400px] bg-gray-100 relative">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.1234567890!2d69.1234567890!3d34.1234567890!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzTCsDIwLjAwMDAwMCwgNjXCsDA3JjAwLjAwMDAwMA!5e0!3m2!1sen!2s!4v1234567890"
+                      src="https://www.google.com/maps?q=Kabul+Polytechnic+University&output=embed"
                       width="100%"
                       height="100%"
                       style={{ border: 0 }}
@@ -107,6 +107,7 @@ const ContactPage = () => {
                       loading="lazy"
                       referrerPolicy="no-referrer-when-downgrade"
                       className="w-full h-full"
+                      title="Kabul Polytechnic University Map"
                     ></iframe>
                   </div>
                   
