f4cce381f67847ed2efb7e02459a8bbe8490ed21
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-15 23:10:57 -0800

    updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -145,84 +145,15 @@ const LoginPage = () => {
                           </label>
                           <div className="relative">
                             <FiCalendar className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                            <select
+                            <input
+                              type="text"
                               name="graduationYear"
                               value={formData.graduationYear || ''}
                               onChange={handleInputChange}
-                              className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg bg-white/50 text-black focus:ring-2 focus:ring-blue-600 focus:border-blue-600 focus:bg-white"
+                              placeholder="Enter graduation year (e.g., 1403)"
+                              className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg bg-white/50 text-black placeholder-gray-700 focus:ring-2 focus:ring-blue-600 focus:border-blue-600 focus:bg-white"
                               required
-                            >
-                              <option value="">Select graduation year</option>
-                              <option value="1403">1403</option>
-                              <option value="1402">1402</option>
-                              <option value="1401">1401</option>
-                              <option value="1400">1400</option>
-                              <option value="1399">1399</option>
-                              <option value="1398">1398</option>
-                              <option value="1397">1397</option>
-                              <option value="1396">1396</option>
-                              <option value="1395">1395</option>
-                              <option value="1394">1394</option>
-                              <option value="1393">1393</option>
-                              <option value="1392">1392</option>
-                              <option value="1391">1391</option>
-                              <option value="1390">1390</option>
-                              <option value="1389">1389</option>
-                              <option value="1388">1388</option>
-                              <option value="1387">1387</option>
-                              <option value="1386">1386</option>
-                              <option value="1385">1385</option>
-                              <option value="1384">1384</option>
-                              <option value="1383">1383</option>
-                              <option value="1382">1382</option>
-                              <option value="1381">1381</option>
-                              <option value="1380">1380</option>
-                              <option value="1379">1379</option>
-                              <option value="1378">1378</option>
-                              <option value="1377">1377</option>
-                              <option value="1376">1376</option>
-                              <option value="1375">1375</option>
-                              <option value="1374">1374</option>
-                              <option value="1373">1373</option>
-                              <option value="1372">1372</option>
-                              <option value="1371">1371</option>
-                              <option value="1370">1370</option>
-                              <option value="1369">1369</option>
-                              <option value="1368">1368</option>
-                              <option value="1367">1367</option>
-                              <option value="1366">1366</option>
-                              <option value="1365">1365</option>
-                              <option value="1364">1364</option>
-                              <option value="1363">1363</option>
-                              <option value="1362">1362</option>
-                              <option value="1361">1361</option>
-                              <option value="1360">1360</option>
-                              <option value="1359">1359</option>
-                              <option value="1358">1358</option>
-                              <option value="1357">1357</option>
-                              <option value="1356">1356</option>
-                              <option value="1355">1355</option>
-                              <option value="1354">1354</option>
-                              <option value="1353">1353</option>
-                              <option value="1352">1352</option>
-                              <option value="1351">1351</option>
-                              <option value="1350">1350</option>
-                              <option value="1349">1349</option>
-                              <option value="1348">1348</option>
-                              <option value="1347">1347</option>
-                              <option value="1346">1346</option>
-                              <option value="1345">1345</option>
-                              <option value="1344">1344</option>
-                              <option value="1343">1343</option>
-                              <option value="1342">1342</option>
-                              <option value="1341">1341</option>
-                              <option value="1340">1340</option>
-                              <option value="1339">1339</option>
-                              <option value="1338">1338</option>
-                              <option value="1337">1337</option>
-                              <option value="1336">1336</option>
-                              <option value="1335">1335</option>
-                            </select>
+                            />
                           </div>
                         </div>
 
