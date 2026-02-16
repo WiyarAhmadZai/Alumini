8d20835c0110701f78a43a3f9a8bd9a07b3abf3e
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-16 11:55:50 -0800

    udpated

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -734,7 +734,7 @@ const ProfilePage = () => {
           <div className="relative flex items-center justify-center ">
           </div>
         </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-12">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 mt-12">
           {loading && (
             <div className="space-y-6">
               {/* Profile Header Skeleton */}
