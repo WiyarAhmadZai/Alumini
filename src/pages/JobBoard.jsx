3a7abf5da7becaf9866e669ea357ef8833035d74
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-18 23:22:12 +0430

    updated

--- src/pages/JobBoard.jsx
+++ src/pages/JobBoard.jsx
@@ -230,7 +230,7 @@ const JobBoard = () => {
 
   const getTypeColor = (type) => {
     switch(type) {
-      case 'Full-time': return 'bg-green-100 text-green-700 dark:bg-green-900/30 dark:text-green-400';
+      case 'Full-time': return 'bg-blue-800 text-white dark:bg-blue-900/30';
       case 'Contract': return 'bg-blue-100 text-blue-700 dark:bg-blue-900/30 dark:text-blue-400';
       case 'Remote': return 'bg-purple-100 text-purple-700 dark:bg-purple-900/30 dark:text-purple-400';
       case 'Part-time': return 'bg-yellow-100 text-yellow-700 dark:bg-yellow-900/30 dark:text-yellow-400';
