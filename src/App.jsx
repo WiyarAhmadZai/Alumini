081243042c2a74d69a0fa5e518ef764a6b931045
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-04-12 16:59:42 +0430

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import DirectoryPage from './pages/DirectoryPage';
 import JobBoard from './pages/JobBoard';
 import JobDetailsPage from './pages/JobDetailsPage';
 import MentorshipPage from './pages/MentorshipPage';
+import MentorDetailPage from './pages/MentorDetailPage';
+import MentorMenteesPage from './pages/MentorMenteesPage';
+import MentorApplicationsPage from './pages/MentorApplicationsPage';
+import MentorRequestsPage from './pages/MentorRequestsPage';
 import EventsPage from './pages/EventsPage';
 import EventDetailPage from './pages/EventDetailPage';
 import RegisteredEventsPage from './pages/RegisteredEventsPage';
@@ -31,6 +35,10 @@ function App() {
           <Route path="/jobs" element={<JobBoard />} />
           <Route path="/job/:id" element={<JobDetailsPage />} />
           <Route path="/mentorship" element={<MentorshipPage />} />
+          <Route path="/mentorship/:id" element={<MentorDetailPage />} />
+          <Route path="/mentorship/my/mentees" element={<MentorMenteesPage />} />
+          <Route path="/mentorship/my/requests" element={<MentorRequestsPage />} />
+          <Route path="/mentorship/my/applications" element={<MentorApplicationsPage />} />
           <Route path="/events" element={<EventsPage />} />
           <Route path="/events/registered" element={<RegisteredEventsPage />} />
           <Route path="/events/:id" element={<EventDetailPage />} />
